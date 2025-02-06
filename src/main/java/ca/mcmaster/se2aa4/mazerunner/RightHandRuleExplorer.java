36fc53574eaa684d58d1c88b924a48b458b54b04
tharnyelilvannan <tharny2005@outlook.com>
2025-02-06 00:55:30 -0500

    Finish explore method (close #5)

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/RightHandRuleExplorer.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/RightHandRuleExplorer.java
@@ -1,11 +1,5 @@
 package main.java.ca.mcmaster.se2aa4.mazerunner;
 
-import main.java.ca.mcmaster.se2aa4.mazerunner.Maze;
-import main.java.ca.mcmaster.se2aa4.mazerunner.Entry;
-import main.java.ca.mcmaster.se2aa4.mazerunner.Exit;
-import main.java.ca.mcmaster.se2aa4.mazerunner.Path;
-import main.java.ca.mcmaster.se2aa4.mazerunner.Explorer;
-
 import java.util.ArrayList;
 
 public class RightHandRuleExplorer extends Explorer {
@@ -28,70 +22,83 @@ public class RightHandRuleExplorer extends Explorer {
         // sets current position as entry point
         int currentRow = entryRow;
         int currentCol = entryCol;
-        Direction direction = Direction.EAST;
+        Direction rightHandWall = Direction.SOUTH;
 
         // sets location for the "fork in the road"
-        int forkRow = entryRow;
-        int forkCol = entryCol;
-        Direction forkDirection = Direction.EAST;
 
         Boolean north;
         Boolean east;
         Boolean south;
         Boolean west;
 
+        // make first move, almost always move forward eastward
+        canonPath = canonPath + "F";
+        currentCol++;
+
         // explore until end of maze
         while (currentRow != exitRow || currentCol != exitCol) {
 
             try {
-                if (currentRow != 0 && currentRow != maze.getMaze().size()) {
+
+                if (currentRow != 0 && currentRow != maze.getMaze().size() && currentCol != 0 && currentCol != maze.getMaze().get(0).size()) {
+
+                    // get the values of each path ahead
                     north = maze.getMaze().get(currentRow - 1).get(currentCol).getCanPass();
                     east = maze.getMaze().get(currentRow).get(currentCol + 1).getCanPass();
                     south = maze.getMaze().get(currentRow + 1).get(currentCol).getCanPass();
                     west = maze.getMaze().get(currentRow).get(currentCol - 1).getCanPass();
 
-                    if (!north && !south && east) {
-                        // go east
+                    if (rightHandWall == Direction.SOUTH && !south && east) {
                         currentCol++;
-
-                        if (direction == Direction.EAST) {
-                            canonPath = canonPath + "F";
-                        }
-                        else if (direction ==  Direction.NORTH) {
-                            direction = compass.turnRight(direction);
-                            canonPath = canonPath + "RF";
-                        }
-                        else if (direction == Direction.SOUTH) {
-                            direction = compass.turnLeft(direction);
-                            canonPath = canonPath + "LF";
-                        }
-                        else if (direction == Direction.WEST) {
-                            direction = compass.turnLeft(direction);
-                            direction = compass.turnLeft(direction);
-                            canonPath = canonPath + "LLF";
-                        }
+                        canonPath = canonPath + "F";
                     }
-                    else if (!south && !east && north) {
-                        // go north
+                    else if (rightHandWall == Direction.NORTH && !north && west) {
+                        currentCol--;
+                        canonPath = canonPath + "F";
+                    }
+                    else if (rightHandWall == Direction.EAST && !east && north) {
                         currentRow--;
-
-                        if (direction == Direction.NORTH) {
-                            canonPath = canonPath + "F";
-                        }
-                        else if (direction == Direction.SOUTH) {
-                            direction = compass.turnRight(direction);
-                            direction = compass.turnRight(direction);
-                            canonPath = canonPath + "RRF";
-
-                        }
-                        else if (direction == Direction.EAST) {
-                            direction = compass.turnLeft(direction);
-                            canonPath = canonPath + "LF";
-                        }
-                        else if (direction == Direction.WEST) {
-                            direction = compass.turnRight(direction);
-                            canonPath = canonPath + "RF";
-                        }
+                        canonPath = canonPath + "F";
+                    }
+                    else if (rightHandWall == Direction.WEST && !west && south) {
+                        currentRow++;
+                        canonPath = canonPath + "F";
+                    }
+                    else if (rightHandWall == Direction.SOUTH && !south && !east) {
+                        canonPath = canonPath + "L";
+                        rightHandWall = Direction.EAST;
+                    }
+                    else if (rightHandWall == Direction.NORTH && !north && !west) {
+                        canonPath = canonPath + "L";
+                        rightHandWall = Direction.WEST;
+                    }
+                    else if (rightHandWall == Direction.EAST && !east && !north) {
+                        canonPath = canonPath + "L";
+                        rightHandWall = Direction.NORTH;
+                    }
+                    else if (rightHandWall == Direction.WEST && !west && !south) {
+                        canonPath = canonPath + "L";
+                        rightHandWall = Direction.SOUTH;
+                    }
+                    else if (rightHandWall == Direction.SOUTH && south) {
+                        canonPath = canonPath + "RF";
+                        rightHandWall = Direction.WEST;
+                        currentRow++;
+                    }
+                    else if (rightHandWall == Direction.NORTH && north) {
+                        canonPath = canonPath + "RF";
+                        rightHandWall = Direction.EAST;
+                        currentRow--;
+                    }
+                    else if (rightHandWall == Direction.EAST && east) {
+                        canonPath = canonPath + "RF";
+                        rightHandWall = Direction.SOUTH;
+                        currentCol++;
+                    }
+                    else if (rightHandWall == Direction.WEST && west) {
+                        canonPath = canonPath + "RF";
+                        rightHandWall = Direction.NORTH;
+                        currentCol--;
                     }
                 }
             } catch (Exception e) {
